cf5ea7155d0a656ec59cde5642579ed88c319b07
Tim Costa <costa.tim@live.be>
2020-03-12 08:52:43 +0100

    updated

--- speedTest.js
+++ speedTest.js
@@ -2,12 +2,13 @@
 var counter = 0;
 var textArea = document.getElementById("texta");
 
-
-textArea.addEventListener("keydown", () => { 
+textArea.addEventListener("keydown", () => {
     if(counter == 0){
         myTimer();
         document.getElementById("ownphrase").disabled = true;
         document.getElementById("btn").disabled = true;
+        document.getElementById("checkVal").className = "text-dark";
+        textArea.className = "form-control";
     }
     counter++;
 })
@@ -18,16 +19,31 @@ function removeElement(elementId) {
     element.parentNode.removeChild(element);
 }
 
+function createAlert(){
+    document.getElementById("alertdanger").innerHTML = `
+                <div id="alertmsg" class="alert alert-danger" role="alert">
+                  Minimum 4 characters
+                </div>`;
+    setTimeout(() => {
+        removeElement("alertmsg");
+    }, 3000);
+
+
+}
+
 function changePhrase(){
     var newPhrase = document.getElementById("ownphrase");
     var oldPhrase  = document.getElementById("phrase");
     var str = newPhrase.value;
-    
+
     if(str.length > 3){
         oldPhrase.innerText = str;
+        removeElement("radiobuttonform");
+        document.getElementById("ownphrasebox").style.margin = "30px auto auto auto";
+    }else{
+        createAlert();
     }
-    removeElement("radiobuttonform");
-    document.getElementById("ownphrasebox").style.margin="80px auto auto auto";
+
 }
 
 
--- timer.js
+++ timer.js
@@ -13,13 +13,24 @@ console.log(phrase.innerText);
 console.log(textAr.value);
 
 var check = document.getElementById("checkVal");
-
+var s = 0;
+var m = 0;
 //var textTyped = textAr.innerText;
 //Each seconds decrement the seconds
 var x = setInterval(function() {
-    mseconds+=1;
+    mseconds += 1;
+    // s = Math.floor(mseconds / 1000);
+    // m = Math.floor(s / 60);
+    // s = s % 60;
+    // m = m % 60;
+    // console.log(s);
+    // console.log(m);
+
+
+
+
     //console.log(mseconds);
-    
+
     if(mseconds > 9){
         mseconds = 0;
         cseconds += 1;
@@ -33,6 +44,9 @@ var x = setInterval(function() {
     if(tseconds == 1000){
         clearInterval(x);
         document.getElementById("message").innerText = "BETTER LUCK NEXT TIME!";
+        document.getElementById("message").className = "text-danger";
+        document.getElementById("texta").className = "form-control is-invalid";
+        document.getElementById("texta").disabled = true;
     }
 
     check.innerText = textAr.value;
@@ -41,7 +55,10 @@ var x = setInterval(function() {
     {
         clearInterval(x);
         document.getElementById("message").innerText = "YOU DID IT!";
+        document.getElementById("message").className = "text-success";
+        document.getElementById("texta").className = "form-control is-valid";
+        document.getElementById("texta").disabled = true;
     }
-}, 1);
+}, 10);
 
 }
